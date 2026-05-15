0e73a263d7fad377bd555cd4af666b600ddfd90b
alaskan-teacup <kondok.toco@gmail.com>
2026-05-15 10:04:39 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,30 +7,30 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 map.on('click', function(e) {
 
   #位置情報の取得
-  alert(
-    "lat: " + e.latlng.lat +
-    "\nlng: " + e.latlng.lng
-  );
-
-});
-
-map.on('click', function(e) {
-
-  console.log(e.latlng.lat, e.latlng.lng);
-
-});
-
-map.on('click', function(e) {
-
-  const marker = L.marker([
-    e.latlng.lat,
-    e.latlng.lng
-  ]).addTo(map);
-
-});
 
 #個々のマーカー
 
 const marker = L.marker([35.0116, 135.7681]).addTo(map);
 
 marker.bindPopup("京都の中心エリア");
+
+       const map = L.map('map').setView([35.0116, 135.7681], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
+
+const marker = L.marker([34.9855, 135.7586]).addTo(map);
+
+marker.bindPopup("京都駅");
+
+
+map.on('click', function(e) {
+
+  alert(
+    "lat: " + e.latlng.lat +
+    "\nlng: " + e.latlng.lng
+  );
+
+});
