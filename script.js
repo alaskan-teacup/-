eebfd7a09a0c608082c500d0438e4289fd6406cc
alaskan-teacup <kondok.toco@gmail.com>
2026-05-15 09:59:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
+map.on('click', function(e) {
+
+  alert(
+    "lat: " + e.latlng.lat +
+    "\nlng: " + e.latlng.lng
+  );
+
+});
+
 const marker = L.marker([35.0116, 135.7681]).addTo(map);
 
 marker.bindPopup("京都の中心エリア");
